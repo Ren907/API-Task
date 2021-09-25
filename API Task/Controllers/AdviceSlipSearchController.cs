dff125542861ee364136850f6ef057a0b13bbbcd
Renald Dalli <renald_dalli@hotmail.com>
2021-09-25 12:05:24 +0200

    Commit 3: Creation of AdviceSlipSearchController and AdviceSlipSearchView

--- API Task/Controllers/AdviceSlipSearchController.cs
+++ API Task/Controllers/AdviceSlipSearchController.cs
@@ -27,6 +27,59 @@ namespace API_Task.Controllers
 
         }
 
-     
+      [HttpPost]
+        public ActionResult AdviceSlipSearchView(string SearchName)
+        {
+            string URL = String.Format("https://api.adviceslip.com/advice/search/");
+            string GetURL = URL + SearchName;
+            WebRequest requestObject = WebRequest.Create(GetURL);
+            requestObject.Method = "GET";
+            HttpWebResponse responseObject = null;
+            responseObject = (HttpWebResponse)requestObject.GetResponse();
+
+            string strResult = null;
+            using (Stream str = responseObject.GetResponseStream())
+            {
+                StreamReader sr = new StreamReader(str);
+                strResult = sr.ReadToEnd();
+                sr.Close();
+            }
+
+            Rootobject obj = JsonConvert.DeserializeObject<Rootobject>(strResult);
+
+
+            Rootobject model = new Rootobject()
+            {
+
+                total_results = obj.total_results,
+                query = obj.query,
+                slips = obj.slips
+
+            };
+
+            foreach (Slip item in obj.slips)
+            {
+                
+                {
+                    slipId = item.id;
+                    slipDate = item.date;
+                    slipAdvice = item.advice;
+
+                }
+            }
+
+            //Slip model = new Slip()
+            //{
+
+            //    id = slipId,
+            //    advice = slipAdvice,
+            //    date = slipDate
+
+            //};
+
+            return View(model);
+         
+
+        }
     }
 }
